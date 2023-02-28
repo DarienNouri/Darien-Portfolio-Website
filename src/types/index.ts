8f59fac53b3d9a164da11255ee7b1d6759bd46bc
DarienNouri <106352088+DarienNouri@users.noreply.github.com>
2023-02-28 01:55:35 -0500

    removing sections for links on about me images

--- src/types/index.ts
+++ src/types/index.ts
@@ -25,8 +25,6 @@ export interface IProject {
   title: string;
   isFeatured: boolean;
   thumbnail: string;
-  githubUrl: string;
-  liveUrl: string;
 }
 export interface IProjects {
   projects: IProject[];
