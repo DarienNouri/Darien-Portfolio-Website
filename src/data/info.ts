d6a008dce2a96f83b1436ce58207550af7302f2d
DarienNouri <dan9232@nyu.edu>
2023-02-27 20:00:41 -0500

    Darien Portfolio Website first push

---
+++ src/data/info.ts
@@ -0,0 +1,116 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Darien Nouri",
+  jobDescription: "Data Scientist",
+  about: `I am a currently a student at New York University dual-majoring in Data Science and Computer Science. I am a results-oriented individual with a 
+  demonstrated track record of designing and developing ML frameworks,
+  data pipelines, and business intelligence tools. 
+  The power in influencing the future is the drive that propels my pursuit to identify, design, and develop 
+  frameworks across the vast uncharted potentials of the data-driven world.
+  When not in the classroom I delegate my time to research in the discovery of applying new technologies to solve real-world problems.
+  Among other things I am also a passionate skier, DJ, pianist, and music producer. 
+  `,
+
+  experience: [
+    {
+      name: "PropertizeAI - Founder & CEO",
+      location: "New York, NY",
+      startDate: "Jan 2023",
+      endDate: "Present",
+      description: [
+      "✓ Developing a platform that identifies emerging real estate markets and provides statistically backed property recommendations to investors and agents.",
+      "✓ Developing and training a GPT-3 fine-tuned model to generate personalized property reports per user case.",
+      "✓ Leading a team of Graduate Researchers to strengthen algorithmic accuracy by identifying predictive features across alternative data sources including social media geolocation trends, transportation data, restaurant health inspection data, traditional real estate predictors, and construction permit.",
+      
+      ],
+    },
+
+    {
+      name: "Predictive Analytics & A.I. Lab - Undergraduate Researcher",
+      location: "New York, NY",
+      startDate: "Jan 2023",
+      endDate: "Present",
+      description: [
+        "✓ Under the direction of Professor Anasse Bari, Undergoing collaborative research connecting multiple alternative data sources to detect early signals and forward insights into the election.",
+        "✓ Collecting and analyzing Google Search Trends and millions of Twitter tweets, Reddit posts, and news headlines related to the 2024 presidential race.",
+        "✓ Applying sentiment analysis algorithms such as Stanford NLP and NLTK to examine attitudes towards nominees.",
+        "✓ Researching novel methods of Sentiment Analysis using GPT-3’s open API with early results demonstrating a 50% increase in sentiment accuracy over NLTK sentiment analysis."
+      ],
+    },
+
+    {
+      name: "Coegil Data Science - Data Engineering Intern",
+      location: "New York, NY",
+      startDate: "Jun 2022",
+      endDate: "Present",
+      description: [
+        "✓	Building SQL-based ETL pipeline system to scrape social media data for current and potential acquisitions, using ML techniques to classify post types and content for successful attributes.",
+        "✓	Building numerous Plotly Dash web-apps used by private equity clients for simplified comparison of asset performance ML predictions."
+
+      ],
+    },
+    {
+      name: "Paramount Real Estate - Data Science Intern",
+      location: "New York, NY",
+      startDate: "Dec 2021",
+      endDate: "Jan 2023",
+      description: [
+        "✓ Developed MS SQL data warehousing framework using custom ETL scripts to extract data from property listing sources.",
+        "✓ Reduced property report preparation time by over 95%.",
+        "✓ Built Streamlit web-app to predict property sale behavior and optimize campaigns, leading to a 25% monthly gain in contracts.",
+      ],
+    },
+    {
+      name: "Sorelle Realty - Real Estate Intern",
+      location: "McLean, VA",
+      startDate: "Oct 2017",
+      endDate: "Sep 2020",
+      description: [
+       
+
+        "✓ Built return and cash flow projection models for investment property procurement, including capital calls, cash-out refi recommendation, and scenario analysis.",
+        "✓ Experimentation with predictive modeling of South Florida submarkets.",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "New York University",
+      location: "New York, NY",
+      startDate: "Aug 2020",
+      endDate: "May 2024",
+      description: [
+        "✓ Majors: Data Science and Computer Science",
+        "✓ Honors: Data Science Club, Delta Kappa Epsilon Social Fraternity, Real Estate Club, Ski & Board Club",
+        "✓ Relevant Courses: Master’s level Big Data Science, Data Science 1 & 2, Linear Algebra, Machine Learning",
+        " Data Structures and Algorithms, Calc 1 & 2, Discrete, Probability & Statistics, Real Estate Development & Entrepreneurship, Micro, Macro"
+      ],
+    },
+    {
+      name: "Woodgrove High School",
+      location: "Purcellville, VA",
+      startDate: "Aug 2016",
+      endDate: "Jun 2020",
+      description: ["✓ Advanced Studies Diploma",
+    ],
+    },
+  ],
+
+  socialMedia: {
+    
+    github: "https://github.com/DarienNouri",
+    email: "nouri.darien@nyu.edu",
+    linkedin: "https://www.linkedin.com/in/darien-nouri/",
+  },
+
+  projects: [
+    // {
+    //   title: "Astro Portfolio",
+    //   isFeatured: true,
+    //   thumbnail: "/assets/images/astro-portfolio.png",
+    //   githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+    //   liveUrl: "https://astro-portfolio-uzair.vercel.app/",
+    // },
+  ],
+};
